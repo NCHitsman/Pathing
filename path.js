d902b75f723012effdde708eb3258db026e28a93
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-05-25 22:39:42 -0700

    targeting functionality

--- path.js
+++ path.js
@@ -17,10 +17,20 @@ new OrbitControls(camera, canvas)
 
 //---------------------------------------------------------------------------
 
-// const cubeGeo = new THREE.BoxGeometry(2000, 2000, 2000);
-// const cubeMat = new THREE.MeshBasicMaterial({ wireframe: true })
-// const cube = new THREE.Mesh(cubeGeo, cubeMat);
-// scene.add(cube)
+
+let target = [-50, 150]
+
+const cubeGeo = new THREE.BoxGeometry(2000, 2000, 2000);
+const cubeMat = new THREE.MeshBasicMaterial({ wireframe: true })
+const cube = new THREE.Mesh(cubeGeo, cubeMat);
+scene.add(cube)
+
+const cubeGeoTwo = new THREE.BoxGeometry(50, 50, 50);
+const cubeMatTwo = new THREE.MeshBasicMaterial({ color: 'red' })
+const cubeTwo = new THREE.Mesh(cubeGeoTwo, cubeMatTwo);
+cubeTwo.position.x = target[0] * 50
+cubeTwo.position.z = target[1] * 50
+scene.add(cubeTwo)
 
 //---------------------------------------------------------------------------
 
@@ -60,28 +70,28 @@ let dirs = ['n', 'e', 's', 'w']
 
 const builder = () => {
 
-    if (!mainDir) {
-        mainDir = dirs[Math.floor(Math.random() * 4)]
-        switch (mainDir) {
-            case 'n':
-                dirs = ['n', 'e', 'w']
-                break;
-            case 'w':
-                dirs = ['n', 's', 'w']
-                break;
-            case 'e':
-                dirs = ['n', 'e', 's']
-                break;
-            case 's':
-                dirs = ['e', 's', 'w']
-                break;
-        }
-    }
+    // if (!mainDir) {
+    //     mainDir = dirs[Math.floor(Math.random() * 4)]
+    //     switch (mainDir) {
+    //         case 'n':
+    //             dirs = ['n', 'e', 'w']
+    //             break;
+    //         case 'w':
+    //             dirs = ['n', 's', 'w']
+    //             break;
+    //         case 'e':
+    //             dirs = ['n', 'e', 's']
+    //             break;
+    //         case 's':
+    //             dirs = ['e', 's', 'w']
+    //             break;
+    //     }
+    // }
 
-    let dir = dirs[Math.floor(Math.random() * 3)]
+    let dir = dirs[Math.floor(Math.random() * 4)]
 
     if (dir == backDir) {
-        dir = dirs[Math.floor(Math.random() * 3)]
+        dir = dirs[Math.floor(Math.random() * 4)]
     }
 
     while (grid.includes(`(${gridX}, ${gridZ + 1})`) && grid.includes(`(${gridX + 1}, ${gridZ})`) && grid.includes(`(${gridX}, ${gridZ - 1})`) && grid.includes(`(${gridX - 1}, ${gridZ})`)) {
@@ -97,44 +107,65 @@ const builder = () => {
             if (grid.includes(`(${gridX}, ${gridZ + 1})`)) {
                 return builder()
             }
+            if (testDirection('Z', '+')) {
+                gridZ += 1
+            } else {
+                return
+            }
             lastZ += 50;
             backDir = 's'
-            gridZ += 1
             grid = grid.concat(`(${gridX}, ${gridZ}); `)
             break;
         case 'e':
             if (grid.includes(`(${gridX + 1}, ${gridZ})`)) {
                 return builder()
             }
+            if (testDirection('X', '+')) {
+                gridX += 1;
+            } else {
+                return
+            }
             lastX += 50;
             backDir = 'w'
-            gridX += 1;
             grid = grid.concat(`(${gridX}, ${gridZ}); `)
             break;
         case 's':
             if (grid.includes(`(${gridX}, ${gridZ - 1})`)) {
                 return builder()
             }
+            if (testDirection('Z', '-')) {
+                gridZ -= 1;
+            } else {
+                return
+            }
             lastZ -= 50;
             backDir = 'n'
-            gridZ -= 1;
             grid = grid.concat(`(${gridX}, ${gridZ}); `)
             break;
         case 'w':
             if (grid.includes(`(${gridX - 1}, ${gridZ})`)) {
                 return builder()
             }
+            if (testDirection('X', '-')) {
+                gridX -= 1;
+            } else {
+                return
+            }
             lastX -= 50;
             backDir = 'e'
-            gridX -= 1;
             grid = grid.concat(`(${gridX}, ${gridZ}); `)
             break;
     }
 
     // console.log(grid)
 
+
     count++
     test()
+
+    if (gridX == target[0] && gridZ == target[1]) {
+        kill()
+    }
 
     createPlane(lastX, lastZ)
 }
@@ -142,9 +173,43 @@ const builder = () => {
 const interval = setInterval(builder, 1);
 
 function test() {
-    if (count > 100) {
+    if (count > 1000) {
         clearInterval(interval)
     }
+}
+
+function testDirection(check, direction) {
+
+    if (check == 'X') {
+        if (direction == '+') {
+            if (gridX + 1 < target[0]) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (gridX - 1 > target[0]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    } else {
+        if (direction == '+') {
+            if (gridZ + 1 < target[1]) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (gridZ - 1 > target[1]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
 }
 
 function kill() {
